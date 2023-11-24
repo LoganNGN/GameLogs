d8b528613b2bc211ccf05aa9c88fd4c6d19dcc2b
SvenRaval <99671825+SvenRaval@users.noreply.github.com>
2023-11-24 16:44:11 +0100

    Feat : API connexion and json conversion (brutal code)

---
+++ GameLogs/APIConnector.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using RawgNET.Manager;
+using RawgNET.Models;
+
+namespace GameLogs
+{
+    internal class APIConnector
+    {
+        private readonly RawgClient client;
+        private readonly string apiKey;
+
+        public APIConnector()
+        {
+            string apiKeyPath = "C:\\Users\\tschi\\source\\repos\\GameLogs\\GameLogs\\apikey.txt";
+            apiKey = File.ReadAllText(apiKeyPath);
+            client = new(new ClientOptions(apiKey));
+        }
+
+        public async Task ProcessGames(string[] gameNames)
+        {
+            foreach (string gameName in gameNames)
+            {
+                if (await client.IsGameExisting(gameName))
+                {
+                    // Fetch detailed information about the game
+                    Game game = await client.GetGame(gameName, true, true);
+
+                    // Convert game data to JSON format
+                    string jsonData = JsonConvert.SerializeObject(game, Formatting.Indented);
+
+                    // Write JSON data to a file
+                    string fileName = $"{gameName}.json";
+                    try
+                    {
+                        using (StreamWriter writer = new StreamWriter(fileName))
+                        {
+                            writer.Write(jsonData);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"Error saving game data to file: {e.Message}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Game '{gameName}' does not exist");
+                }
+            }
+        }
+    }
+}
--- GameLogs/Program.cs
+++ GameLogs/Program.cs
@@ -3,14 +3,24 @@ namespace GameLogs
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            // Initialize application configuration
             ApplicationConfiguration.Initialize();
+
+            // Create an instance of the APIConnector class
+            APIConnector connector = new APIConnector();
+
+            // Define an array of game names to process
+            string[] gameNames = new string[] { "overwatch", "fortnite", "apex_legends" };
+
+            // Process the games and save their data to JSON files
+            connector.ProcessGames(gameNames);
+
+            // Run the Windows Forms application
             Application.Run(new Form1());
         }
     }
