10d0b5c42630213b35739dae29e5d0ee7ef229b6
Raphaël Robert <pf31etl@eduvaud.ch>
2024-01-10 10:30:21 +0100

    erreur recherche API pour GUI

--- GameLogs/AcceuilForm.cs
+++ GameLogs/AcceuilForm.cs
@@ -1,78 +1,63 @@
+using static APIConnector;
+
 namespace GameLogs
 {
     public partial class Acceuil : Form
     {
         #region private attributes
-        private testinfoGames _testinfoGames = null;
+        private readonly APIConnector apiConnector = new APIConnector();
+        private string _searchText = null;
         #endregion private attributes
 
         #region public attributes
-        public string _searchText;
+
         #endregion public attributes
 
         public Acceuil()
         {
             InitializeComponent();
         }
-
-        // test liste à suprimmer
-        private List<testinfoGames> CreateListContact()
+        private async void btmRecherche_Click(object sender, EventArgs e)
         {
-            List<testinfoGames> testinfoGamesList = new List<testinfoGames>
+            _searchText = tbRecherche.Text.Trim();
+
+            if (!string.IsNullOrEmpty(_searchText))
             {
-                new testinfoGames("Ricard", "Nya nya nya nay nay naynaynaynanyany", "console.jpeg"),
-                new testinfoGames("Nyaa", " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim", "Nya4.jpg"),
-            };
-
-            return testinfoGamesList;
-        }
-
-        private bool SearchGames(string searchText, out testinfoGames foundGame)
-        {
-            List<testinfoGames> gamesList = CreateListContact();
-
-            foreach (testinfoGames testinfoGames in gamesList)
-            {
-                if (testinfoGames.Name == searchText)
+                try
                 {
-                    foundGame = testinfoGames;
-                    return true;
+                    Dictionary<string, object> gameData = await apiConnector.GetGameData(_searchText, "Games");
+                    await apiConnector.ProcessGames(new string[] { _searchText });
+                    MessageBox.Show($"Recherche réussie pour le jeu : {_searchText}");
+
+                    // Utilisez le dictionnaire gameData comme vous le souhaitez
+                    // Par exemple, vous pouvez l'utiliser pour mettre à jour votre interface utilisateur (UI)
+                    UpdateGui(gameData);
                 }
-            }
-            foundGame = null;
-            return false;
-        }
-
-        private void btmRecherche_Click(object sender, EventArgs e)
-        {
-            _searchText = tbRecherche.Text;
-
-            testinfoGames foundGame;
-            bool isGameFound = SearchGames(_searchText, out foundGame);
-
-            if (isGameFound)
-            {
-                _testinfoGames = foundGame;
-                UpdateGui();
-                gbGame.Visible = true;
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erreur lors de la recherche pour le jeu {_searchText}: {ex.Message}");
+                }
             }
             else
             {
-                MessageBox.Show("Jeu non trouvé", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Veuillez entrer un nom de jeu valide.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
-        private void btmDescription_Click(object sender, EventArgs e)
+        private void UpdateGui(Dictionary<string, object> gameData)
         {
-            descriptionGame descriptionGameForm = new descriptionGame(_testinfoGames);
-            descriptionGameForm.ShowDialog();
-        }
-
-        private void UpdateGui()
-        {
-            if (_testinfoGames != null)
+            if (gameData != null)
             {
-                this.lbTitle.Text = _testinfoGames.Name;
+                // Assurez-vous que "Name" existe dans le dictionnaire avant d'y accéder
+                if (gameData.ContainsKey("Name"))
+                {
+                    this.lbTitle.Text = gameData["Name"].ToString();
+                }
+                else
+                {
+                    // Traitez le cas où "Name" n'existe pas dans le dictionnaire
+                    this.lbTitle.Text = "Nom non disponible";
+                }
             }
         }
     }
